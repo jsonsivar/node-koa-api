68b197d1b37cd47d47187c00a8e1c7dd893cc006
jsonsivar <jsonsivar@gmail.com>
2018-02-13 04:37:54 -0500

    added first koa route

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,17 @@
-console.log('It works!');
+import Koa from "koa";
+
+const app = new Koa;
+const PORT = process.env.PORT || 8080;
+
+app.use(async (ctx) => {
+	ctx.body = {
+		status: "success",
+		message: "hello, world!"
+	};
+});
+
+const server = app.listen(PORT, () => {
+	console.log(`Server listening on port: ${ PORT }`);
+});
+
+module.exports = server;
